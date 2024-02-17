9d78d56d20cb8eb7f7caece9c250611ce2120016
amechkarov <mechkarov.alexander@gmail.com>
2024-02-17 14:30:07 +0200

    Fixed error when creating post

--- src/main/java/com/example/forum/helpers/PostMapper.java
+++ src/main/java/com/example/forum/helpers/PostMapper.java
@@ -1,5 +1,6 @@
 package com.example.forum.helpers;
 
+import com.example.forum.models.Comment;
 import com.example.forum.models.Post;
 import com.example.forum.models.dtos.PostDto;
 import com.example.forum.services.PostService;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class PostMapper {
@@ -30,6 +33,7 @@ public class PostMapper {
         post.setTitle(postDto.getTitle());
         post.setContent(postDto.getContent());
         post.setDateAndTimeOfCreation(LocalDateTime.now());
+        post.setPostComments(new HashSet<>());
         return post;
     }
 
